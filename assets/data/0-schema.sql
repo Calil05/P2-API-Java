25755b72d099ad7b2c58705cc1d6d6b4578245f9
Gabriel Calil <103449368+Calil05@users.noreply.github.com>
2024-11-17 16:45:57 -0300

    fix: general fixes

--- assets/data/0-schema.sql
+++ assets/data/0-schema.sql
@@ -1,6 +1,6 @@
 USE application;
 
-CREATE TABLE users (
+CREATE TABLE usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
